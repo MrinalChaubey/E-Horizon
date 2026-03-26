c93caa93047e456fc9f5698b5f17f390c75d8f10
IshantSingh27 <ishantsingh0602@gmail.com>
2026-03-26 10:22:32 +0530

    User Dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,10 @@ import Home from './pages/Home';
 import EventDetail from './pages/EventDetail';
 import Login from './pages/Login';
 import Register from './pages/Register';
-// import UserDashboard from './pages/UserDashboard';
+import UserDashboard from './pages/UserDashboard';
 // import AdminDashboard from './pages/AdminDashboard';
-// import PaymentSuccess from './pages/PaymentSuccess';
-// import PaymentFailed from './pages/PaymentFailed';
+import PaymentSuccess from './pages/PaymentSuccess';
+import PaymentFailed from './pages/PaymentFailed';
 
 function App() {
     return (
@@ -21,10 +21,10 @@ function App() {
                     <Route path="/events/:id" element={<EventDetail />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
-                    {/* <Route path="/dashboard" element={<UserDashboard />} /> */}
+                    <Route path="/dashboard" element={<UserDashboard />} />
                     {/* <Route path="/admin" element={<AdminDashboard />} /> */}
-                    {/* <Route path="/payment-success" element={<PaymentSuccess />} /> */}
-                    {/* <Route path="/payment-failed" element={<PaymentFailed />} /> */}
+                    <Route path="/payment-success" element={<PaymentSuccess />} />
+                    <Route path="/payment-failed" element={<PaymentFailed />} />
 
                     <Route
                         path="*"
